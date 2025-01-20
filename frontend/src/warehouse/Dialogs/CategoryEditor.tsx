85b7b743836586dc83d34fb0bfaeb585f43560e3
danielniemiec <danielniemiec44@gmail.com>
2025-01-20 18:36:48 +0100

    Added working category add modal fields in CategoryEditor.tsx

--- frontend/src/warehouse/Dialogs/CategoryEditor.tsx
+++ frontend/src/warehouse/Dialogs/CategoryEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {HTMLInputTypeAttribute, useState} from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
@@ -22,6 +22,7 @@ import AddNewItemButton from '../Modules/AddNewItemButton';
 import Stack from '@mui/material/Stack';
 
 interface CategoryFieldTypes {
+    id: number;
     name: string;
     type: string;
     defaultValue: number;
@@ -29,18 +30,29 @@ interface CategoryFieldTypes {
 }
 
 const CategoryEditor: React.FC = () => {
-
-
-    const fields = ["field1", "field2", "field3"];
-
-
     const { t } = useTranslation();
     const dispatch = useDispatch();
     const editCategoryId = useSelector((state: RootReducerTypes) => state.warehouse.editCategoryId);
+    const [fieldEditIndex, setFieldEditIndex] = useState<number>(0);
+    const [fieldsFromBackend, setFieldsFromBackend] = useState<CategoryFieldTypes[]>(
+        [
+            {
+                id: 1,
+                name: "Example",
+                type: "Example type",
+                defaultValue: 0,
+                maxLen: 0,
+            }
+        ]
+    );
 
-    const [fieldTypes, setFieldTypes] = useState<CategoryFieldTypes[]>([
-        { value: 'string', label: 'String' },
-    ]);
+    const enabledInputFields: Partial<Record<keyof CategoryFieldTypes, HTMLInputTypeAttribute>> =
+        {
+            name: "text",
+            type: "text",
+            defaultValue: "number",
+            maxLen: "number",
+        };
 
     const handleClose = () => {
         dispatch({ type: 'CLOSE_EDIT_CATEGORY_MODAL' });
@@ -48,13 +60,16 @@ const CategoryEditor: React.FC = () => {
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
-        switch (name) {
-            case 'name':
-                setFieldTypes({ ...fieldTypes, name: value });         
-                break;
-            default:
-                break;
+        console.log(name, value);
+        setFieldsFromBackend((prevFields) => {
+            return prevFields.map((field, index) => {
+                if (index === fieldEditIndex) {
+                    return { ...field, [name]: value };
+                }
+                return field;
+            });
         }
+        );
     }
 
     return (
@@ -67,8 +82,8 @@ const CategoryEditor: React.FC = () => {
                         <Grid item xs={6}>
                             <List>
                                 <AddNewItemButton />
-                                {fields.map((field) => (
-                                    <ListItem style={{ backgroundColor: grey[200], marginBottom: '10px' }} key={field}>
+                                {fieldsFromBackend.map(field => (
+                                    <ListItem style={{ backgroundColor: grey[200], marginBottom: '10px' }} key={field.id}>
                                         <Stack spacing={1} direction="row" sx={{ alignItems: 'center'}}>
                                             <Tooltip title={t('fieldsActions.remove')} arrow>
                                                 <IconButton size="small" color="error" aria-label="remove">
@@ -80,7 +95,7 @@ const CategoryEditor: React.FC = () => {
                                                     <ModeIcon />
                                                 </IconButton>
                                             </Tooltip>
-                                            <Typography noWrap>{field}</Typography>
+                                            <Typography noWrap>{field.name}</Typography>
                                         </Stack>
                                     </ListItem>
                                 ))}
@@ -92,10 +107,10 @@ const CategoryEditor: React.FC = () => {
                                     autoFocus
                                     margin="dense"
                                     label={t('tooltips.categoryName')}
-                                    type="text"
+                                    type={enabledInputFields.name}
                                     fullWidth
                                     variant="outlined"
-                                    value={name}
+                                    value={fieldsFromBackend[fieldEditIndex].name}
                                     onChange={handleChange}
                                     name={"name"}
                                 />
@@ -106,11 +121,12 @@ const CategoryEditor: React.FC = () => {
                                     autoFocus
                                     margin="dense"
                                     label={t('tooltips.categoryType')}
-                                    type="text"
+                                    type={enabledInputFields.type}
                                     fullWidth
                                     variant="outlined"
                                     onChange={handleChange}
-                                    name={"name"}
+                                    value={fieldsFromBackend[fieldEditIndex].type}
+                                    name={"type"}
                                 />
                             </Tooltip>
 
@@ -119,10 +135,12 @@ const CategoryEditor: React.FC = () => {
                                     autoFocus
                                     margin="dense"
                                     label={t('tooltips.maxLen')}
-                                    type="number"
+                                    type={enabledInputFields.maxLen}
                                     fullWidth
                                     variant="outlined"
                                     onChange={handleChange}
+                                    value={fieldsFromBackend[fieldEditIndex].maxLen}
+                                    name={"maxLen"}
                                 />
                             </Tooltip>
 
@@ -131,10 +149,12 @@ const CategoryEditor: React.FC = () => {
                                     autoFocus
                                     margin="dense"
                                     label={t('tooltips.defaultValue')}
-                                    type="number"
+                                    type={enabledInputFields.defaultValue}
                                     fullWidth
                                     variant="outlined"
                                     onChange={handleChange}
+                                    value={fieldsFromBackend[fieldEditIndex].defaultValue}
+                                    name={"defaultValue"}
                                 />
                             </Tooltip>
                         </Grid>
@@ -148,7 +168,7 @@ const CategoryEditor: React.FC = () => {
                 </Button>
                 <Button
                     color="primary"
-                    disabled={!name}
+                    disabled={!fieldsFromBackend.every(field => field.name)}
                 >
                     {t('dialogActions.confirm')}
                 </Button>
